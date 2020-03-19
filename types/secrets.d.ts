3fe06a5ea767b08b7fc27b644aa6a6e600db1a6a
danikaze <danikaze@gmail.com>
2020-03-19 21:58:27 +0900

    Reorganize TS declarations

---
+++ types/secrets.d.ts
@@ -0,0 +1,11 @@
+/*
+ * Constants from `secret.js`, only available in server side
+ */
+declare const FIREBASE_APIKEY: string;
+declare const FIREBASE_AUTHDOMAIN: string;
+declare const FIREBASE_DATABASEURL: string;
+declare const FIREBASE_PROJECTID: string;
+declare const FIREBASE_STORAGEBUCKET: string;
+declare const FIREBASE_MESSAGINGSENDERID: string;
+declare const FIREBASE_APPID: string;
+declare const FIREBASE_MEASUREMENTID: string;
