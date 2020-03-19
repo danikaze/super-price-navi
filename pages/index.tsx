b693414c45aa6e5276f188d5fbc159f129d7a6fc
danikaze <danikaze@gmail.com>
2020-03-19 21:59:48 +0900

    Provide apiCall method based on axios

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import { GetServerSideProps, NextPage } from 'next';
-import { request, RequestOptions } from 'http';
+import { apiCall } from 'util/api-call';
 
 const Index: NextPage = props => {
   const envList = Object.entries(props).map(([key, value]) => (
@@ -19,35 +19,10 @@ const Index: NextPage = props => {
 };
 
 export const getServerSideProps: GetServerSideProps = async () => {
-  return new Promise(resolve => {
-    const options: RequestOptions = {
-      hostname: 'localhost',
-      port: SERVER_PORT,
-      path: '/api/env',
-      method: 'GET',
+  return apiCall('env').then(res => {
+    return {
+      props: res.data,
     };
-
-    const req = request(options, res => {
-      let data = '';
-
-      res.on('data', d => {
-        data += d.toString();
-      });
-
-      res.on('end', () => {
-        let props = {};
-        try {
-          props = JSON.parse(data);
-        } finally {
-          resolve({ props });
-        }
-      });
-    });
-
-    req.on('error', () => {
-      resolve({ props: {} });
-    });
-    req.end();
   });
 };
 
